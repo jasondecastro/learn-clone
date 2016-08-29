c4cac9c09a51b47564ce5e7245afadba1e12d975
dillonmcroberts <dillon.mcroberts@gmail.com>
2016-08-29 09:53:00 -0400

    build lesson page links

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,19 @@ dilson = {
   password: "dilson123"
 }
 
-Lesson.delete_all
-Lesson.create(name: "Rails Loops")
-Lesson.create(name: "Rails Conditionals")
+
 
 Track.delete_all
-Track.create(name: "Web Development Immersive")
+track1 = Track.create(name: "Web Development Immersive")
 
 User.delete_all
 user = User.create(dilson)
-user.track = Track.all.first
+user.track = track1
+
+Lesson.delete_all
+lesson1 = Lesson.create(name: "Rails Loops", content: "Show how to loop over an array and print each number inside")
+lesson1.track = track1
+lesson1.save
+lesson2 = Lesson.create(name: "Rails Conditionals", content: "Show how to use 'if', 'and', and 'else'")
+lesson2.track = track1
+lesson2.save
