80754b396d764a796efaa983f86edb9e1be19ae7
Jason Decastro <jasonrdecastro@gmail.com>
2016-08-29 11:52:40 -0400

    added email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+require 'pony'
 class UsersController < ApplicationController
   before_action :authorize, except: [:new, :create]
 
@@ -17,6 +18,24 @@ class UsersController < ApplicationController
     @user.track = Track.all.first
     if @user.save
       session[:user_id] = @user.id
+
+      Pony.mail({
+      :to => @user.email,
+      :via => :smtp,
+      :via_options => {
+          :address              => 'smtp.gmail.com',
+          :port                 => '587',
+          :enable_starttls_auto => true,
+          :user_name            => 'no.reply.zoltar@gmail.com',
+          :password             => 'zoltar123',
+          :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+          :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+        },
+        :from => 'no.reply.zoltar@gmail.com',
+        :subject => 'Thanks for signing up to Learn.',
+        :body => "Here's your fortune below. I'll see you again when the night is dark and full of terrors."
+      })
+
       redirect_to user_path(@user)
     else
       redirect_to signup_path
